863f44f7f0bba9b1947a114923514951ec6a0dc2
Matt Michael <matthewemichael@gmail.com>
2019-10-19 17:21:58 -0500

    application wil display all of the items available for sale

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,6 +19,27 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  console.log("Connection Established");
+  console.log("\nConnection Established\n");
+  displayInventory();
 });
 
+function displayInventory() {
+	queryStr = "SELECT * FROM products";
+	connection.query(queryStr,function(err,data) {
+		if (err) throw err;
+		console.log("================================================== Bamazon Inventory ==================================================\n");
+		console.log("=======================================================================================================================\n");
+
+		var inventory = '';
+		for (var i = 0; i < data.length; i++) {
+			inventory = '';
+			inventory += "Item ID: " + data[i].item_id + " | ";
+			inventory += "Product Name: " + data[i].product_name + " | ";
+			inventory += "Department: " + data[i].department_name + " | ";
+			inventory += "Price: $" + data[i].price + "\n";
+			console.log(inventory);
+		}
+		console.log("=======================================================================================================================\n");
+
+	}) 
+};
