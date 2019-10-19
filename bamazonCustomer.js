800b475dfbc87808183619c4e74f27f0af23a7a9
Matt Michael <matthewemichael@gmail.com>
2019-10-19 16:33:08 -0500

    created js file, connecting to mysql database

---
+++ bamazonCustomer.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("Connection Established");
+});
+
